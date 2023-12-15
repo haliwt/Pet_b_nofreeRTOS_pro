af394ef7ae702b1b8a29564e7a8908f407f40d2d
haliwt <jonewang@126.com>
2023-12-15 19:35:22 +0800

    Update TIM14 need initial function. is error .

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -38,9 +38,9 @@ void MX_TIM14_Init(void)
 
   /* USER CODE END TIM14_Init 1 */
   htim14.Instance = TIM14;
-  htim14.Init.Prescaler = 0;
+  htim14.Init.Prescaler = 23;
   htim14.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim14.Init.Period = 65535;
+  htim14.Init.Period = 9999; //10ms
   htim14.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim14.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim14) != HAL_OK)
